ccbf4c846593782ba0b55c259423824cb04924a4
martincaisley <martin.caisley@northumbria.ac.uk>
2020-12-02 14:31:58 +0000

    Added Medical Questionnaire

---
+++ Team29 Group Project/Team29 Group Project/MedicalQuestionnaireGUI.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Team29_Group_Project
+{
+    partial class MedicalQuestionnaireGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBL_medTitle = new System.Windows.Forms.Label();
+            this.LBL_medConditions = new System.Windows.Forms.Label();
+            this.TXT_medicalConditions = new System.Windows.Forms.TextBox();
+            this.LBL_medication = new System.Windows.Forms.Label();
+            this.TXT_medication = new System.Windows.Forms.TextBox();
+            this.LBL_alllergies = new System.Windows.Forms.Label();
+            this.TXT_allergies = new System.Windows.Forms.TextBox();
+            this.LBL_treatmentPlan = new System.Windows.Forms.Label();
+            this.CMB_treatmentPlan = new System.Windows.Forms.ComboBox();
+            this.BTN_submitPatient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBL_medTitle
+            // 
+            this.LBL_medTitle.AutoSize = true;
+            this.LBL_medTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_medTitle.Location = new System.Drawing.Point(248, 23);
+            this.LBL_medTitle.Name = "LBL_medTitle";
+            this.LBL_medTitle.Size = new System.Drawing.Size(306, 33);
+            this.LBL_medTitle.TabIndex = 0;
+            this.LBL_medTitle.Text = "Medical Questionnaire";
+            // 
+            // LBL_medConditions
+            // 
+            this.LBL_medConditions.AutoSize = true;
+            this.LBL_medConditions.Location = new System.Drawing.Point(3, 133);
+            this.LBL_medConditions.Name = "LBL_medConditions";
+            this.LBL_medConditions.Size = new System.Drawing.Size(135, 13);
+            this.LBL_medConditions.TabIndex = 1;
+            this.LBL_medConditions.Text = "Existing Medical Conditions";
+            // 
+            // TXT_medicalConditions
+            // 
+            this.TXT_medicalConditions.Location = new System.Drawing.Point(144, 130);
+            this.TXT_medicalConditions.Name = "TXT_medicalConditions";
+            this.TXT_medicalConditions.Size = new System.Drawing.Size(169, 20);
+            this.TXT_medicalConditions.TabIndex = 2;
+            // 
+            // LBL_medication
+            // 
+            this.LBL_medication.AutoSize = true;
+            this.LBL_medication.Location = new System.Drawing.Point(3, 187);
+            this.LBL_medication.Name = "LBL_medication";
+            this.LBL_medication.Size = new System.Drawing.Size(98, 13);
+            this.LBL_medication.TabIndex = 3;
+            this.LBL_medication.Text = "Existing Medication";
+            // 
+            // TXT_medication
+            // 
+            this.TXT_medication.Location = new System.Drawing.Point(144, 184);
+            this.TXT_medication.Name = "TXT_medication";
+            this.TXT_medication.Size = new System.Drawing.Size(169, 20);
+            this.TXT_medication.TabIndex = 4;
+            // 
+            // LBL_alllergies
+            // 
+            this.LBL_alllergies.AutoSize = true;
+            this.LBL_alllergies.Location = new System.Drawing.Point(3, 241);
+            this.LBL_alllergies.Name = "LBL_alllergies";
+            this.LBL_alllergies.Size = new System.Drawing.Size(85, 13);
+            this.LBL_alllergies.TabIndex = 5;
+            this.LBL_alllergies.Text = "Existing Allergies";
+            // 
+            // TXT_allergies
+            // 
+            this.TXT_allergies.Location = new System.Drawing.Point(144, 238);
+            this.TXT_allergies.Name = "TXT_allergies";
+            this.TXT_allergies.Size = new System.Drawing.Size(169, 20);
+            this.TXT_allergies.TabIndex = 6;
+            // 
+            // LBL_treatmentPlan
+            // 
+            this.LBL_treatmentPlan.AutoSize = true;
+            this.LBL_treatmentPlan.Location = new System.Drawing.Point(3, 288);
+            this.LBL_treatmentPlan.Name = "LBL_treatmentPlan";
+            this.LBL_treatmentPlan.Size = new System.Drawing.Size(79, 13);
+            this.LBL_treatmentPlan.TabIndex = 7;
+            this.LBL_treatmentPlan.Text = "Treatment Plan";
+            // 
+            // CMB_treatmentPlan
+            // 
+            this.CMB_treatmentPlan.FormattingEnabled = true;
+            this.CMB_treatmentPlan.Items.AddRange(new object[] {
+            "Band 1 ",
+            "Band 2",
+            "Band 3",
+            "Free Treatment"});
+            this.CMB_treatmentPlan.Location = new System.Drawing.Point(144, 288);
+            this.CMB_treatmentPlan.Name = "CMB_treatmentPlan";
+            this.CMB_treatmentPlan.Size = new System.Drawing.Size(169, 21);
+            this.CMB_treatmentPlan.TabIndex = 8;
+            // 
+            // BTN_submitPatient
+            // 
+            this.BTN_submitPatient.Location = new System.Drawing.Point(282, 440);
+            this.BTN_submitPatient.Name = "BTN_submitPatient";
+            this.BTN_submitPatient.Size = new System.Drawing.Size(218, 65);
+            this.BTN_submitPatient.TabIndex = 9;
+            this.BTN_submitPatient.Text = "Add Patient";
+            this.BTN_submitPatient.UseVisualStyleBackColor = true;
+            // 
+            // MedicalQuestionnaireGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 541);
+            this.Controls.Add(this.BTN_submitPatient);
+            this.Controls.Add(this.CMB_treatmentPlan);
+            this.Controls.Add(this.LBL_treatmentPlan);
+            this.Controls.Add(this.TXT_allergies);
+            this.Controls.Add(this.LBL_alllergies);
+            this.Controls.Add(this.TXT_medication);
+            this.Controls.Add(this.LBL_medication);
+            this.Controls.Add(this.TXT_medicalConditions);
+            this.Controls.Add(this.LBL_medConditions);
+            this.Controls.Add(this.LBL_medTitle);
+            this.Name = "MedicalQuestionnaireGUI";
+            this.Text = "MedicalQuestionnaireGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBL_medTitle;
+        private System.Windows.Forms.Label LBL_medConditions;
+        private System.Windows.Forms.TextBox TXT_medicalConditions;
+        private System.Windows.Forms.Label LBL_medication;
+        private System.Windows.Forms.TextBox TXT_medication;
+        private System.Windows.Forms.Label LBL_alllergies;
+        private System.Windows.Forms.TextBox TXT_allergies;
+        private System.Windows.Forms.Label LBL_treatmentPlan;
+        private System.Windows.Forms.ComboBox CMB_treatmentPlan;
+        private System.Windows.Forms.Button BTN_submitPatient;
+    }
+}
--- Team29 Group Project/Team29 Group Project/PatientRegistration.Designer.cs
+++ Team29 Group Project/Team29 Group Project/PatientRegistration.Designer.cs
@@ -28,33 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.LBL_patTitle = new System.Windows.Forms.Label();
             this.LBL_fname = new System.Windows.Forms.Label();
             this.LBL_lname = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.LBL_DoB = new System.Windows.Forms.Label();
             this.LBL_address = new System.Windows.Forms.Label();
             this.LBL_email = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.LBL_occupation = new System.Windows.Forms.Label();
-            this.BTN_fullTime = new System.Windows.Forms.RadioButton();
-            this.BTN_partTime = new System.Windows.Forms.RadioButton();
-            this.BTN_education = new System.Windows.Forms.RadioButton();
-            this.BTN_partTimeEducation = new System.Windows.Forms.RadioButton();
-            this.BTN_unemployed = new System.Windows.Forms.RadioButton();
+            this.PNL_occupations = new System.Windows.Forms.Panel();
             this.BTN_LIbenefits = new System.Windows.Forms.RadioButton();
-            this.panel1.SuspendLayout();
+            this.BTN_unemployed = new System.Windows.Forms.RadioButton();
+            this.BTN_partTimeEducation = new System.Windows.Forms.RadioButton();
+            this.BTN_education = new System.Windows.Forms.RadioButton();
+            this.BTN_partTime = new System.Windows.Forms.RadioButton();
+            this.BTN_fullTime = new System.Windows.Forms.RadioButton();
+            this.LBL_occupation = new System.Windows.Forms.Label();
+            this.TXT_fname = new System.Windows.Forms.TextBox();
+            this.TXT_sname = new System.Windows.Forms.TextBox();
+            this.TXT_address = new System.Windows.Forms.TextBox();
+            this.TXT_email = new System.Windows.Forms.TextBox();
+            this.BTN_medQuestionnaire = new System.Windows.Forms.Button();
+            this.PNL_occupations.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // LBL_patTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(264, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(295, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Patient Registration";
+            this.LBL_patTitle.AutoSize = true;
+            this.LBL_patTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_patTitle.Location = new System.Drawing.Point(264, 29);
+            this.LBL_patTitle.Name = "LBL_patTitle";
+            this.LBL_patTitle.Size = new System.Drawing.Size(295, 37);
+            this.LBL_patTitle.TabIndex = 0;
+            this.LBL_patTitle.Text = "Patient Registration";
             // 
             // LBL_fname
             // 
@@ -80,7 +85,7 @@
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(117, 197);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(79, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(136, 20);
             this.dateTimePicker1.TabIndex = 3;
             // 
             // LBL_DoB
@@ -113,83 +118,18 @@
             this.LBL_email.TabIndex = 6;
             this.LBL_email.Text = "Email";
             // 
-            // panel1
+            // PNL_occupations
             // 
-            this.panel1.Controls.Add(this.BTN_LIbenefits);
-            this.panel1.Controls.Add(this.BTN_unemployed);
-            this.panel1.Controls.Add(this.BTN_partTimeEducation);
-            this.panel1.Controls.Add(this.BTN_education);
-            this.panel1.Controls.Add(this.BTN_partTime);
-            this.panel1.Controls.Add(this.BTN_fullTime);
-            this.panel1.Location = new System.Drawing.Point(16, 343);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(161, 208);
-            this.panel1.TabIndex = 7;
-            // 
-            // LBL_occupation
-            // 
-            this.LBL_occupation.AutoSize = true;
-            this.LBL_occupation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LBL_occupation.Location = new System.Drawing.Point(12, 320);
-            this.LBL_occupation.Name = "LBL_occupation";
-            this.LBL_occupation.Size = new System.Drawing.Size(90, 20);
-            this.LBL_occupation.TabIndex = 8;
-            this.LBL_occupation.Text = "Occupation";
-            // 
-            // BTN_fullTime
-            // 
-            this.BTN_fullTime.AutoSize = true;
-            this.BTN_fullTime.Location = new System.Drawing.Point(3, 12);
-            this.BTN_fullTime.Name = "BTN_fullTime";
-            this.BTN_fullTime.Size = new System.Drawing.Size(122, 17);
-            this.BTN_fullTime.TabIndex = 0;
-            this.BTN_fullTime.TabStop = true;
-            this.BTN_fullTime.Text = "Full time employment";
-            this.BTN_fullTime.UseVisualStyleBackColor = true;
-            // 
-            // BTN_partTime
-            // 
-            this.BTN_partTime.AutoSize = true;
-            this.BTN_partTime.Location = new System.Drawing.Point(3, 45);
-            this.BTN_partTime.Name = "BTN_partTime";
-            this.BTN_partTime.Size = new System.Drawing.Size(125, 17);
-            this.BTN_partTime.TabIndex = 1;
-            this.BTN_partTime.TabStop = true;
-            this.BTN_partTime.Text = "Part time employment";
-            this.BTN_partTime.UseVisualStyleBackColor = true;
-            // 
-            // BTN_education
-            // 
-            this.BTN_education.AutoSize = true;
-            this.BTN_education.Location = new System.Drawing.Point(3, 78);
-            this.BTN_education.Name = "BTN_education";
-            this.BTN_education.Size = new System.Drawing.Size(113, 17);
-            this.BTN_education.TabIndex = 2;
-            this.BTN_education.TabStop = true;
-            this.BTN_education.Text = "Full time education";
-            this.BTN_education.UseVisualStyleBackColor = true;
-            // 
-            // BTN_partTimeEducation
-            // 
-            this.BTN_partTimeEducation.AutoSize = true;
-            this.BTN_partTimeEducation.Location = new System.Drawing.Point(3, 112);
-            this.BTN_partTimeEducation.Name = "BTN_partTimeEducation";
-            this.BTN_partTimeEducation.Size = new System.Drawing.Size(116, 17);
-            this.BTN_partTimeEducation.TabIndex = 3;
-            this.BTN_partTimeEducation.TabStop = true;
-            this.BTN_partTimeEducation.Text = "Part time education";
-            this.BTN_partTimeEducation.UseVisualStyleBackColor = true;
-            // 
-            // BTN_unemployed
-            // 
-            this.BTN_unemployed.AutoSize = true;
-            this.BTN_unemployed.Location = new System.Drawing.Point(0, 149);
-            this.BTN_unemployed.Name = "BTN_unemployed";
-            this.BTN_unemployed.Size = new System.Drawing.Size(84, 17);
-            this.BTN_unemployed.TabIndex = 4;
-            this.BTN_unemployed.TabStop = true;
-            this.BTN_unemployed.Text = "Unemployed";
-            this.BTN_unemployed.UseVisualStyleBackColor = true;
+            this.PNL_occupations.Controls.Add(this.BTN_LIbenefits);
+            this.PNL_occupations.Controls.Add(this.BTN_unemployed);
+            this.PNL_occupations.Controls.Add(this.BTN_partTimeEducation);
+            this.PNL_occupations.Controls.Add(this.BTN_education);
+            this.PNL_occupations.Controls.Add(this.BTN_partTime);
+            this.PNL_occupations.Controls.Add(this.BTN_fullTime);
+            this.PNL_occupations.Location = new System.Drawing.Point(16, 343);
+            this.PNL_occupations.Name = "PNL_occupations";
+            this.PNL_occupations.Size = new System.Drawing.Size(161, 208);
+            this.PNL_occupations.TabIndex = 7;
             // 
             // BTN_LIbenefits
             // 
@@ -202,24 +142,131 @@
             this.BTN_LIbenefits.Text = "Low Income Benefits";
             this.BTN_LIbenefits.UseVisualStyleBackColor = true;
             // 
+            // BTN_unemployed
+            // 
+            this.BTN_unemployed.AutoSize = true;
+            this.BTN_unemployed.Location = new System.Drawing.Point(0, 149);
+            this.BTN_unemployed.Name = "BTN_unemployed";
+            this.BTN_unemployed.Size = new System.Drawing.Size(84, 17);
+            this.BTN_unemployed.TabIndex = 4;
+            this.BTN_unemployed.TabStop = true;
+            this.BTN_unemployed.Text = "Unemployed";
+            this.BTN_unemployed.UseVisualStyleBackColor = true;
+            // 
+            // BTN_partTimeEducation
+            // 
+            this.BTN_partTimeEducation.AutoSize = true;
+            this.BTN_partTimeEducation.Location = new System.Drawing.Point(3, 112);
+            this.BTN_partTimeEducation.Name = "BTN_partTimeEducation";
+            this.BTN_partTimeEducation.Size = new System.Drawing.Size(116, 17);
+            this.BTN_partTimeEducation.TabIndex = 3;
+            this.BTN_partTimeEducation.TabStop = true;
+            this.BTN_partTimeEducation.Text = "Part time education";
+            this.BTN_partTimeEducation.UseVisualStyleBackColor = true;
+            // 
+            // BTN_education
+            // 
+            this.BTN_education.AutoSize = true;
+            this.BTN_education.Location = new System.Drawing.Point(3, 78);
+            this.BTN_education.Name = "BTN_education";
+            this.BTN_education.Size = new System.Drawing.Size(113, 17);
+            this.BTN_education.TabIndex = 2;
+            this.BTN_education.TabStop = true;
+            this.BTN_education.Text = "Full time education";
+            this.BTN_education.UseVisualStyleBackColor = true;
+            // 
+            // BTN_partTime
+            // 
+            this.BTN_partTime.AutoSize = true;
+            this.BTN_partTime.Location = new System.Drawing.Point(3, 45);
+            this.BTN_partTime.Name = "BTN_partTime";
+            this.BTN_partTime.Size = new System.Drawing.Size(125, 17);
+            this.BTN_partTime.TabIndex = 1;
+            this.BTN_partTime.TabStop = true;
+            this.BTN_partTime.Text = "Part time employment";
+            this.BTN_partTime.UseVisualStyleBackColor = true;
+            // 
+            // BTN_fullTime
+            // 
+            this.BTN_fullTime.AutoSize = true;
+            this.BTN_fullTime.Location = new System.Drawing.Point(3, 12);
+            this.BTN_fullTime.Name = "BTN_fullTime";
+            this.BTN_fullTime.Size = new System.Drawing.Size(122, 17);
+            this.BTN_fullTime.TabIndex = 0;
+            this.BTN_fullTime.TabStop = true;
+            this.BTN_fullTime.Text = "Full time employment";
+            this.BTN_fullTime.UseVisualStyleBackColor = true;
+            // 
+            // LBL_occupation
+            // 
+            this.LBL_occupation.AutoSize = true;
+            this.LBL_occupation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_occupation.Location = new System.Drawing.Point(12, 320);
+            this.LBL_occupation.Name = "LBL_occupation";
+            this.LBL_occupation.Size = new System.Drawing.Size(90, 20);
+            this.LBL_occupation.TabIndex = 8;
+            this.LBL_occupation.Text = "Occupation";
+            // 
+            // TXT_fname
+            // 
+            this.TXT_fname.Location = new System.Drawing.Point(104, 109);
+            this.TXT_fname.Name = "TXT_fname";
+            this.TXT_fname.Size = new System.Drawing.Size(149, 20);
+            this.TXT_fname.TabIndex = 9;
+            // 
+            // TXT_sname
+            // 
+            this.TXT_sname.Location = new System.Drawing.Point(104, 154);
+            this.TXT_sname.Name = "TXT_sname";
+            this.TXT_sname.Size = new System.Drawing.Size(149, 20);
+            this.TXT_sname.TabIndex = 10;
+            // 
+            // TXT_address
+            // 
+            this.TXT_address.Location = new System.Drawing.Point(104, 238);
+            this.TXT_address.Name = "TXT_address";
+            this.TXT_address.Size = new System.Drawing.Size(149, 20);
+            this.TXT_address.TabIndex = 11;
+            // 
+            // TXT_email
+            // 
+            this.TXT_email.Location = new System.Drawing.Point(104, 278);
+            this.TXT_email.Name = "TXT_email";
+            this.TXT_email.Size = new System.Drawing.Size(149, 20);
+            this.TXT_email.TabIndex = 12;
+            // 
+            // BTN_medQuestionnaire
+            // 
+            this.BTN_medQuestionnaire.Location = new System.Drawing.Point(310, 605);
+            this.BTN_medQuestionnaire.Name = "BTN_medQuestionnaire";
+            this.BTN_medQuestionnaire.Size = new System.Drawing.Size(158, 54);
+            this.BTN_medQuestionnaire.TabIndex = 15;
+            this.BTN_medQuestionnaire.Text = "Medical Questionnaire";
+            this.BTN_medQuestionnaire.UseVisualStyleBackColor = true;
+            // 
             // PatientRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 576);
+            this.ClientSize = new System.Drawing.Size(800, 671);
+            this.Controls.Add(this.BTN_medQuestionnaire);
+            this.Controls.Add(this.TXT_email);
+            this.Controls.Add(this.TXT_address);
+            this.Controls.Add(this.TXT_sname);
+            this.Controls.Add(this.TXT_fname);
             this.Controls.Add(this.LBL_occupation);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PNL_occupations);
             this.Controls.Add(this.LBL_email);
             this.Controls.Add(this.LBL_address);
             this.Controls.Add(this.LBL_DoB);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.LBL_lname);
             this.Controls.Add(this.LBL_fname);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LBL_patTitle);
             this.Name = "PatientRegistration";
             this.Text = "PatientRegistration";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.PNL_occupations.ResumeLayout(false);
+            this.PNL_occupations.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,14 +274,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBL_patTitle;
         private System.Windows.Forms.Label LBL_fname;
         private System.Windows.Forms.Label LBL_lname;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label LBL_DoB;
         private System.Windows.Forms.Label LBL_address;
         private System.Windows.Forms.Label LBL_email;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel PNL_occupations;
         private System.Windows.Forms.RadioButton BTN_fullTime;
         private System.Windows.Forms.Label LBL_occupation;
         private System.Windows.Forms.RadioButton BTN_education;
@@ -242,5 +289,10 @@
         private System.Windows.Forms.RadioButton BTN_LIbenefits;
         private System.Windows.Forms.RadioButton BTN_unemployed;
         private System.Windows.Forms.RadioButton BTN_partTimeEducation;
+        private System.Windows.Forms.TextBox TXT_fname;
+        private System.Windows.Forms.TextBox TXT_sname;
+        private System.Windows.Forms.TextBox TXT_address;
+        private System.Windows.Forms.TextBox TXT_email;
+        private System.Windows.Forms.Button BTN_medQuestionnaire;
     }
 }

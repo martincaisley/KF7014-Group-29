4a76ceac65963f4393d560a2b9bca69e17109bc5
DrummondHeckels <74360947+DrummondHeckels@users.noreply.github.com>
2020-12-03 14:46:01 +0000

    Update NewAppointment.Designer.cs

--- AssessmentIdeas V2/AssessmentIdeas/NewAppointment.Designer.cs
+++ AssessmentIdeas V2/AssessmentIdeas/NewAppointment.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace AssessmentIdeas
 {
     partial class NewAppointment
@@ -37,81 +37,257 @@ namespace AssessmentIdeas
             this.lbl_date = new System.Windows.Forms.Label();
             this.lbl_time = new System.Windows.Forms.Label();
             this.btn_addAppointment = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxDentalVeneers = new System.Windows.Forms.CheckBox();
+            this.checkBoxTeethWhitening = new System.Windows.Forms.CheckBox();
+            this.checkBoxWisdomTooth = new System.Windows.Forms.CheckBox();
+            this.checkBoxDentalImplants = new System.Windows.Forms.CheckBox();
+            this.checkBoxDentures = new System.Windows.Forms.CheckBox();
+            this.checkBoxBrokenTooth = new System.Windows.Forms.CheckBox();
+            this.checkBoxBraces = new System.Windows.Forms.CheckBox();
+            this.checkBoxScaleAndPolish = new System.Windows.Forms.CheckBox();
+            this.checkBoxRootCanal = new System.Windows.Forms.CheckBox();
+            this.checkBoxFillings = new System.Windows.Forms.CheckBox();
+            this.checkBoxCrowns = new System.Windows.Forms.CheckBox();
+            this.checkBoxBridges = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbl_title
             // 
             this.lbl_title.AutoSize = true;
             this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
-            this.lbl_title.Location = new System.Drawing.Point(12, 9);
+            this.lbl_title.Location = new System.Drawing.Point(9, 7);
+            this.lbl_title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_title.Name = "lbl_title";
-            this.lbl_title.Size = new System.Drawing.Size(326, 46);
+            this.lbl_title.Size = new System.Drawing.Size(265, 37);
             this.lbl_title.TabIndex = 0;
             this.lbl_title.Text = "Add Appointment";
             // 
             // txt_name
             // 
-            this.txt_name.Location = new System.Drawing.Point(140, 84);
+            this.txt_name.Location = new System.Drawing.Point(105, 68);
+            this.txt_name.Margin = new System.Windows.Forms.Padding(2);
             this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(100, 22);
+            this.txt_name.Size = new System.Drawing.Size(76, 20);
             this.txt_name.TabIndex = 1;
             // 
             // txt_time
             // 
-            this.txt_time.Location = new System.Drawing.Point(140, 140);
+            this.txt_time.Location = new System.Drawing.Point(105, 114);
+            this.txt_time.Margin = new System.Windows.Forms.Padding(2);
             this.txt_time.Name = "txt_time";
-            this.txt_time.Size = new System.Drawing.Size(100, 22);
+            this.txt_time.Size = new System.Drawing.Size(76, 20);
             this.txt_time.TabIndex = 2;
             // 
             // txt_date
             // 
-            this.txt_date.Location = new System.Drawing.Point(140, 112);
+            this.txt_date.Location = new System.Drawing.Point(105, 91);
+            this.txt_date.Margin = new System.Windows.Forms.Padding(2);
             this.txt_date.Name = "txt_date";
-            this.txt_date.Size = new System.Drawing.Size(100, 22);
+            this.txt_date.Size = new System.Drawing.Size(76, 20);
             this.txt_date.TabIndex = 3;
             // 
             // lbl_name
             // 
             this.lbl_name.AutoSize = true;
-            this.lbl_name.Location = new System.Drawing.Point(63, 84);
+            this.lbl_name.Location = new System.Drawing.Point(47, 68);
+            this.lbl_name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_name.Name = "lbl_name";
-            this.lbl_name.Size = new System.Drawing.Size(45, 17);
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
             this.lbl_name.TabIndex = 4;
             this.lbl_name.Text = "Name";
             // 
             // lbl_date
             // 
             this.lbl_date.AutoSize = true;
-            this.lbl_date.Location = new System.Drawing.Point(63, 115);
+            this.lbl_date.Location = new System.Drawing.Point(47, 93);
+            this.lbl_date.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_date.Name = "lbl_date";
-            this.lbl_date.Size = new System.Drawing.Size(38, 17);
+            this.lbl_date.Size = new System.Drawing.Size(30, 13);
             this.lbl_date.TabIndex = 5;
             this.lbl_date.Text = "Date";
             // 
             // lbl_time
             // 
             this.lbl_time.AutoSize = true;
-            this.lbl_time.Location = new System.Drawing.Point(63, 145);
+            this.lbl_time.Location = new System.Drawing.Point(47, 118);
+            this.lbl_time.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_time.Name = "lbl_time";
-            this.lbl_time.Size = new System.Drawing.Size(39, 17);
+            this.lbl_time.Size = new System.Drawing.Size(30, 13);
             this.lbl_time.TabIndex = 6;
             this.lbl_time.Text = "Time";
             // 
             // btn_addAppointment
             // 
-            this.btn_addAppointment.Location = new System.Drawing.Point(66, 178);
+            this.btn_addAppointment.Location = new System.Drawing.Point(63, 299);
+            this.btn_addAppointment.Margin = new System.Windows.Forms.Padding(2);
             this.btn_addAppointment.Name = "btn_addAppointment";
-            this.btn_addAppointment.Size = new System.Drawing.Size(174, 23);
+            this.btn_addAppointment.Size = new System.Drawing.Size(130, 26);
             this.btn_addAppointment.TabIndex = 7;
             this.btn_addAppointment.Text = "Add Appointment";
             this.btn_addAppointment.UseVisualStyleBackColor = true;
             this.btn_addAppointment.Click += new System.EventHandler(this.btn_addAppointment_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxDentalVeneers);
+            this.groupBox1.Controls.Add(this.checkBoxTeethWhitening);
+            this.groupBox1.Controls.Add(this.checkBoxWisdomTooth);
+            this.groupBox1.Controls.Add(this.checkBoxDentalImplants);
+            this.groupBox1.Controls.Add(this.checkBoxDentures);
+            this.groupBox1.Controls.Add(this.checkBoxBrokenTooth);
+            this.groupBox1.Controls.Add(this.checkBoxBraces);
+            this.groupBox1.Controls.Add(this.checkBoxScaleAndPolish);
+            this.groupBox1.Controls.Add(this.checkBoxRootCanal);
+            this.groupBox1.Controls.Add(this.checkBoxFillings);
+            this.groupBox1.Controls.Add(this.checkBoxCrowns);
+            this.groupBox1.Controls.Add(this.checkBoxBridges);
+            this.groupBox1.Location = new System.Drawing.Point(12, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 155);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Appointment Type";
+            // 
+            // checkBoxDentalVeneers
+            // 
+            this.checkBoxDentalVeneers.AutoSize = true;
+            this.checkBoxDentalVeneers.Location = new System.Drawing.Point(142, 135);
+            this.checkBoxDentalVeneers.Name = "checkBoxDentalVeneers";
+            this.checkBoxDentalVeneers.Size = new System.Drawing.Size(99, 17);
+            this.checkBoxDentalVeneers.TabIndex = 11;
+            this.checkBoxDentalVeneers.Text = "Dental Veneers";
+            this.checkBoxDentalVeneers.UseVisualStyleBackColor = true;
+            this.checkBoxDentalVeneers.CheckedChanged += new System.EventHandler(this.checkBoxDentalVeneers_CheckedChanged);
+            // 
+            // checkBoxTeethWhitening
+            // 
+            this.checkBoxTeethWhitening.AutoSize = true;
+            this.checkBoxTeethWhitening.Location = new System.Drawing.Point(142, 112);
+            this.checkBoxTeethWhitening.Name = "checkBoxTeethWhitening";
+            this.checkBoxTeethWhitening.Size = new System.Drawing.Size(102, 17);
+            this.checkBoxTeethWhitening.TabIndex = 10;
+            this.checkBoxTeethWhitening.Text = "Teeth whitening";
+            this.checkBoxTeethWhitening.UseVisualStyleBackColor = true;
+            this.checkBoxTeethWhitening.CheckedChanged += new System.EventHandler(this.checkBoxTeethWhitening_CheckedChanged);
+            // 
+            // checkBoxWisdomTooth
+            // 
+            this.checkBoxWisdomTooth.AutoSize = true;
+            this.checkBoxWisdomTooth.Location = new System.Drawing.Point(142, 20);
+            this.checkBoxWisdomTooth.Name = "checkBoxWisdomTooth";
+            this.checkBoxWisdomTooth.Size = new System.Drawing.Size(95, 17);
+            this.checkBoxWisdomTooth.TabIndex = 9;
+            this.checkBoxWisdomTooth.Text = "Wisdom Tooth";
+            this.checkBoxWisdomTooth.UseVisualStyleBackColor = true;
+            this.checkBoxWisdomTooth.CheckedChanged += new System.EventHandler(this.checkBoxWisdomTooth_CheckedChanged);
+            // 
+            // checkBoxDentalImplants
+            // 
+            this.checkBoxDentalImplants.AutoSize = true;
+            this.checkBoxDentalImplants.Location = new System.Drawing.Point(142, 43);
+            this.checkBoxDentalImplants.Name = "checkBoxDentalImplants";
+            this.checkBoxDentalImplants.Size = new System.Drawing.Size(99, 17);
+            this.checkBoxDentalImplants.TabIndex = 8;
+            this.checkBoxDentalImplants.Text = "Dental Implants";
+            this.checkBoxDentalImplants.UseVisualStyleBackColor = true;
+            this.checkBoxDentalImplants.CheckedChanged += new System.EventHandler(this.checkBoxDentalImplants_CheckedChanged);
+            // 
+            // checkBoxDentures
+            // 
+            this.checkBoxDentures.AutoSize = true;
+            this.checkBoxDentures.Location = new System.Drawing.Point(142, 66);
+            this.checkBoxDentures.Name = "checkBoxDentures";
+            this.checkBoxDentures.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxDentures.TabIndex = 7;
+            this.checkBoxDentures.Text = "Dentures";
+            this.checkBoxDentures.UseVisualStyleBackColor = true;
+            this.checkBoxDentures.CheckedChanged += new System.EventHandler(this.checkBoxDentures_CheckedChanged);
+            // 
+            // checkBoxBrokenTooth
+            // 
+            this.checkBoxBrokenTooth.AutoSize = true;
+            this.checkBoxBrokenTooth.Location = new System.Drawing.Point(142, 89);
+            this.checkBoxBrokenTooth.Name = "checkBoxBrokenTooth";
+            this.checkBoxBrokenTooth.Size = new System.Drawing.Size(91, 17);
+            this.checkBoxBrokenTooth.TabIndex = 6;
+            this.checkBoxBrokenTooth.Text = "Broken Tooth";
+            this.checkBoxBrokenTooth.UseVisualStyleBackColor = true;
+            this.checkBoxBrokenTooth.CheckedChanged += new System.EventHandler(this.checkBoxBrokenTooth_CheckedChanged);
+            // 
+            // checkBoxBraces
+            // 
+            this.checkBoxBraces.AutoSize = true;
+            this.checkBoxBraces.Location = new System.Drawing.Point(6, 135);
+            this.checkBoxBraces.Name = "checkBoxBraces";
+            this.checkBoxBraces.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxBraces.TabIndex = 5;
+            this.checkBoxBraces.Text = "Braces";
+            this.checkBoxBraces.UseVisualStyleBackColor = true;
+            this.checkBoxBraces.CheckedChanged += new System.EventHandler(this.checkBoxBraces_CheckedChanged);
+            // 
+            // checkBoxScaleAndPolish
+            // 
+            this.checkBoxScaleAndPolish.AutoSize = true;
+            this.checkBoxScaleAndPolish.Location = new System.Drawing.Point(6, 112);
+            this.checkBoxScaleAndPolish.Name = "checkBoxScaleAndPolish";
+            this.checkBoxScaleAndPolish.Size = new System.Drawing.Size(104, 17);
+            this.checkBoxScaleAndPolish.TabIndex = 4;
+            this.checkBoxScaleAndPolish.Text = "Scale and polish";
+            this.checkBoxScaleAndPolish.UseVisualStyleBackColor = true;
+            this.checkBoxScaleAndPolish.CheckedChanged += new System.EventHandler(this.checkBoxScaleAndPolish_CheckedChanged_1);
+            // 
+            // checkBoxRootCanal
+            // 
+            this.checkBoxRootCanal.AutoSize = true;
+            this.checkBoxRootCanal.Location = new System.Drawing.Point(6, 89);
+            this.checkBoxRootCanal.Name = "checkBoxRootCanal";
+            this.checkBoxRootCanal.Size = new System.Drawing.Size(130, 17);
+            this.checkBoxRootCanal.TabIndex = 3;
+            this.checkBoxRootCanal.Text = "Root Canal Treatment";
+            this.checkBoxRootCanal.UseVisualStyleBackColor = true;
+            this.checkBoxRootCanal.CheckedChanged += new System.EventHandler(this.checkBoxRootCanal_CheckedChanged);
+            // 
+            // checkBoxFillings
+            // 
+            this.checkBoxFillings.AutoSize = true;
+            this.checkBoxFillings.Location = new System.Drawing.Point(6, 66);
+            this.checkBoxFillings.Name = "checkBoxFillings";
+            this.checkBoxFillings.Size = new System.Drawing.Size(57, 17);
+            this.checkBoxFillings.TabIndex = 2;
+            this.checkBoxFillings.Text = "Fillings";
+            this.checkBoxFillings.UseVisualStyleBackColor = true;
+            this.checkBoxFillings.CheckedChanged += new System.EventHandler(this.checkBoxFillings_CheckedChanged);
+            // 
+            // checkBoxCrowns
+            // 
+            this.checkBoxCrowns.AutoSize = true;
+            this.checkBoxCrowns.Location = new System.Drawing.Point(6, 43);
+            this.checkBoxCrowns.Name = "checkBoxCrowns";
+            this.checkBoxCrowns.Size = new System.Drawing.Size(61, 17);
+            this.checkBoxCrowns.TabIndex = 1;
+            this.checkBoxCrowns.Text = "Crowns";
+            this.checkBoxCrowns.UseVisualStyleBackColor = true;
+            this.checkBoxCrowns.CheckedChanged += new System.EventHandler(this.checkBoxCrowns_CheckedChanged);
+            // 
+            // checkBoxBridges
+            // 
+            this.checkBoxBridges.AutoSize = true;
+            this.checkBoxBridges.Location = new System.Drawing.Point(6, 20);
+            this.checkBoxBridges.Name = "checkBoxBridges";
+            this.checkBoxBridges.Size = new System.Drawing.Size(61, 17);
+            this.checkBoxBridges.TabIndex = 0;
+            this.checkBoxBridges.Text = "Bridges";
+            this.checkBoxBridges.UseVisualStyleBackColor = true;
+            this.checkBoxBridges.CheckedChanged += new System.EventHandler(this.checkBoxBridges_CheckedChanged);
+            // 
             // NewAppointment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 228);
+            this.ClientSize = new System.Drawing.Size(271, 329);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_addAppointment);
             this.Controls.Add(this.lbl_time);
             this.Controls.Add(this.lbl_date);
@@ -120,9 +296,12 @@ namespace AssessmentIdeas
             this.Controls.Add(this.txt_time);
             this.Controls.Add(this.txt_name);
             this.Controls.Add(this.lbl_title);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "NewAppointment";
             this.Text = "NewAppointment";
             this.Load += new System.EventHandler(this.NewAppointment_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,5 +317,18 @@ namespace AssessmentIdeas
         private System.Windows.Forms.Label lbl_date;
         private System.Windows.Forms.Label lbl_time;
         private System.Windows.Forms.Button btn_addAppointment;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxRootCanal;
+        private System.Windows.Forms.CheckBox checkBoxFillings;
+        private System.Windows.Forms.CheckBox checkBoxCrowns;
+        private System.Windows.Forms.CheckBox checkBoxBridges;
+        private System.Windows.Forms.CheckBox checkBoxBraces;
+        private System.Windows.Forms.CheckBox checkBoxScaleAndPolish;
+        private System.Windows.Forms.CheckBox checkBoxDentalVeneers;
+        private System.Windows.Forms.CheckBox checkBoxTeethWhitening;
+        private System.Windows.Forms.CheckBox checkBoxWisdomTooth;
+        private System.Windows.Forms.CheckBox checkBoxDentalImplants;
+        private System.Windows.Forms.CheckBox checkBoxDentures;
+        private System.Windows.Forms.CheckBox checkBoxBrokenTooth;
     }
 }

a4c2d16d4cfab9e31f98fd8bcc4a9cfc26cdbb99
martincaisley <martin.caisley@northumbria.ac.uk>
2020-12-04 11:43:13 +0000

    Added patient class

---
+++ Team29 Group Project/Team29 Group Project/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team29_Group_Project
+{
+    class Patient
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public DateTime DoB { get; set; }
+        public String email { get; set; }
+        public String occupation { get; set; }
+        public String GPname { get; set; }
+        public String GPaddress { get; set; }
+    }
+}

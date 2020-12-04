482d048355c71a832a9d9bd3d5d8dd2aff76840e
markburns14 <74359653+markburns14@users.noreply.github.com>
2020-12-04 11:34:15 +0000

    Added MyDBEntities and entity framework

---
+++ Team29 Group Project/Team29 Group Project/MyDBEntities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team29_Group_Project
+{
+    class MyDBEntities : DbContext
+    {
+        public MyDBEntities()
+            : base("name=conString")
+        {
+        }
+
+        //public DbSet<Patient> Patients { get; set; }
+        //public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
+
+

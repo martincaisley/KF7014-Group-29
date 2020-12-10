3cd4c5725f1729e64f2b566df8ace807fddb6f90
martincaisley <martin.caisley@northumbria.ac.uk>
2020-12-10 14:52:56 +0000

    Added validation to create the different patient types based on the form inputs

--- Team29 Group Project/Team29 Group Project/IPatientRegistrationGUI.cs
+++ Team29 Group Project/Team29 Group Project/IPatientRegistrationGUI.cs
@@ -8,5 +8,14 @@ namespace Team29_Group_Project
 {
     interface IPatientRegistrationGUI
     {
+        String GetFirstName();
+        String GetLastName();
+        DateTime GetDoB();
+        String GetAddress();
+        String GetEmail();
+        String GetPhoneNumber();
+        String GetOccupation();
+        String GetGPname();
+        String GetGPaddress();
     }
 }

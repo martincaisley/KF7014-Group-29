84cbd2d552cf44eb1467ccea9896061b6a17d6c0
Mark Burns <w16016013@northumbria.ac.uk>
2020-12-30 11:16:35 +0000

    Added reference to repository pattern test

--- Team29 Group Project/Team29TestProject/RepositoryTest.cs
+++ Team29 Group Project/Team29TestProject/RepositoryTest.cs
@@ -12,14 +12,18 @@ namespace Team29TestProject
     public class RepositoryTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddToRepository()
         {
-           
+            /*
+             * code to test the repository object creation from:
+             * https://rubikscode.net/2018/04/16/implementing-and-testing-repository-pattern-using-entity-framework/
+             */
 
             var testObject = new Appointment();
 
             var context = new Mock<MyDBEntities>();
             var dbSetMock = new Mock<DbSet<Appointment>>();
+
             context.Setup(x => x.Set<Appointment>()).Returns(dbSetMock.Object);
             dbSetMock.Setup(x => x.Add(It.IsAny<Appointment>())).Returns(testObject);
 
@@ -28,7 +32,6 @@ namespace Team29TestProject
 
             context.Verify(x => x.Set<Appointment>());
             dbSetMock.Verify(x => x.Add(It.Is<Appointment>(y => y == testObject)));
-
         }
     }
 }
